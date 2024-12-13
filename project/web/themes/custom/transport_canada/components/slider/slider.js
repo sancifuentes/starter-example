88b2e89cc0800ba0da45bc4909e6d345af48f21f
Santiago Cifuentes <santico32@hotmail.com>
2024-12-13 20:24:18 +0000

    Theming with SDC Class 3

---
+++ project/web/themes/custom/transport_canada/components/slider/slider.js
@@ -0,0 +1,34 @@
+const swiper = new Swiper('.swiper', {
+    // Optional parameters
+    // direction: 'vertical',
+    loop: true,
+  
+    // If we need pagination
+    pagination: {
+      el: '.swiper-pagination',
+    },
+  
+    // Navigation arrows
+    navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev',
+    },
+  
+    // And if we need scrollbar
+    scrollbar: {
+      el: '.swiper-scrollbar',
+    },
+
+    slidesPerView: 1,
+
+    breakpoints: {
+        460: {
+            slidesPerView: 2,
+            spaceBetween: 15,
+        },
+        // 960: {
+        //     slidesPerView: 3,
+        //     spaceBetween: 30
+        // }
+    }
+  });
--- project/web/themes/custom/transport_canada/tailwind.config.js
+++ project/web/themes/custom/transport_canada/tailwind.config.js
@@ -2,7 +2,11 @@
 module.exports = {
   content: ['./{templates,components}/**/*.{js,twig}'],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        'tilt-neon': '"Tilt Neon", sans-serif;'
+      }
+    },
   },
   plugins: [],
 }
